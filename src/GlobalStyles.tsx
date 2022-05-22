eef8af632afb4615086b0b68c7b669cdc40673cb
Maciejlys <maciej.lyszczarz98@gmail.com>
2022-05-22 12:00:47 +0200

    Cleaning up init project

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 80%;
+  max-width: 1500px;
+  margin: 0 auto;
+`;
+
+export const FlexCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 1.3rem;
+}
+
+img {
+  max-width: 100%;
+}
+`;
---
+++ src/components/pages/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface MainPageProps {}
+
+export const MainPage: React.FC<MainPageProps> = () => {
+  return <>Hi</>;
+};
