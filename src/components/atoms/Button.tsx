9d39e47573f37be8369e39cedf77e7431f669fb8
Maciejlys <maciej.lyszczarz98@gmail.com>
2022-05-23 02:15:00 +0200

    Button and title fixes

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -6,7 +6,17 @@ interface ButtonProps {
   children: React.ReactNode;
 }
 
-const ButtonStyle = styled.button``;
+const ButtonStyle = styled.button`
+  font-size: 18px;
+  font-weight: 900;
+  padding: 1.1rem 1rem;
+  background-color: #c4c4c4;
+  outline: none;
+  border: none;
+  :hover {
+    background-color: #b3b3b3;
+  }
+`;
 
 export const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
   return <ButtonStyle onClick={onClick}>{children}</ButtonStyle>;
--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -10,11 +10,32 @@ interface TitleStyleProps {
   size: number;
 }
 
-export const TitleStyle = styled.h1<TitleStyleProps>`
-  font-size: ${(props) => props.size}px;
+const TitleStyle = styled.h1<TitleStyleProps>`
+  margin-top: 20%;
   font-family: "Antonio", sans-serif;
+  font-size: ${(props) => props.size}px;
+
+  @media (max-width: 1200px) {
+    font-size: ${(props) => props.size * 0.8}px;
+  }
+
+  @media (max-width: 1000px) {
+    font-size: ${(props) => props.size * 0.6}px;
+    margin-top: 10%;
+  } ;
+`;
+
+const Center = styled.div`
+  @media (max-width: 900px) {
+    display: flex;
+    justify-content: center;
+  } ;
 `;
 
 export const Title: React.FC<TitleProps> = ({ size, children }) => {
-  return <TitleStyle size={size}>{children}</TitleStyle>;
+  return (
+    <Center>
+      <TitleStyle size={size}>{children}</TitleStyle>
+    </Center>
+  );
 };
